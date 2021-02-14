4983edd3eb0cac7499ae6c8144104e2b275f0a98
mahsn <mhsnaty@gmail.com>
2021-02-13 22:40:11 -0300

    fix pets

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar/Navbar.js';
 import Home from './components/Pages/Home/Home.js';
-import Products from './components/Pages/Pets/Pets.js';
+import Pets from './components/Pages/Pets/Pets.js';
 import SingLogMain from './components/Pages/SingLogMain/SingLogMain.js';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
           <Navbar/>
           <Switch>
             <Route  path="/" exact component = {Home}/>
-            <Route  path="/pets" exact component = {Products}/>
+            <Route  path="/pets" exact component = {Pets}/>
             <SingLogMain  path="/sign-up" exact component  = {SingLogMain} />
           </Switch>
       </Router>
